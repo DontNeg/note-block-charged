a002450981ea2df853f62b4639833878ca7042db
LMokahel <lukianmokahel123@gmail.com>
2024-03-28 00:26:23 -0700

    Updating deprecated methods and REI

--- src/main/java/dontneg/noteblockcharged/block/NoteBlocks.java
+++ src/main/java/dontneg/noteblockcharged/block/NoteBlocks.java
@@ -4,6 +4,7 @@ import dontneg.noteblockcharged.NoteBlockCharged;
 import dontneg.noteblockcharged.block.cnoteblock.ChargedNoteBlock;
 import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
@@ -15,7 +16,7 @@ import net.minecraft.util.Identifier;
 
 public class NoteBlocks {
     public static final Block CHARGED_NOTE_BLOCK = registerBlock("charged_note_block",
-            new ChargedNoteBlock(FabricBlockSettings.copyOf(Blocks.NOTE_BLOCK)
+            new ChargedNoteBlock(AbstractBlock.Settings.copy(Blocks.NOTE_BLOCK)
                     .luminance(state -> state.get(ChargedNoteBlock.CHARGED) ? 7 : 0)));
 
 
